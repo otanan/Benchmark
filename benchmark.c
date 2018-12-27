022e56844577b7402e85b604c90250a28ca9302c
Jonathan Delgado <jonathandelgado0327@gmail.com>
2018-12-27 13:16:53 -0500

    Slight refactoring of variable names and including units and following standard C conventions.

--- benchmark.c
+++ benchmark.c
@@ -4,7 +4,7 @@
 
 //Function that takes in another function from a library
 //and uses it to perform a shrewd benchmark
-double benchmarkGeneral(void (*test)(void)) {
+double benchmark_general(void (*test)(void)) {
 	//Converting factor to measure in milliseconds
 	const double SECS_TO_MILLI = 1000.0;
 	double start, end;
@@ -13,38 +13,38 @@ double benchmarkGeneral(void (*test)(void)) {
 
 	(*test)();
 
-	end = clock() * SECS_TO_MILLI / CLOCKS_PER_SEC;
+	end = clock() * SECS_TO_MILLI / CLOCKS_PER_SEC;	
 
 	//Some output padding
 	printf("\n\n");
 
 	printf("Start time: %f ms\nEnd time: %f ms\n", start, end);
-	double timeElapsed = end - start;
-	printf("Time elapsed: %f ms\n", timeElapsed);
+	double time_elapsed_ms = end - start;
+	printf("Time elapsed: %f ms\n", time_elapsed_ms);
 
 	//Some output padding
 	printf("\n\n");
 
-	return timeElapsed;
+	return time_elapsed_ms;
 }
 
-void benchmarkGeneralMulti(void (*test)(void), int runs) {
+void benchmark_general_multi(void (*test)(void), int runs) {
 	//Array holding the time entries for each time elapsed
 	double times[runs];
 	//Total time for averaging
-	double totalTime = 0;
+	double total_time_ms = 0;
 
 	for(int i = 0; i < runs; i++) {
-		times[i] = benchmarkGeneral(test);
-		totalTime += times[i];
+		times[i] = benchmark_general(test);
+		total_time_ms += times[i];
 	}
 
 	//Some output padding
 	printf("\n\n");
 
 	printf("Total amount of runs: %d\n", runs);
-	printf("Total time elapsed: %f ms\n", totalTime);
-	printf("Average time elapsed: %.10f ms\n", totalTime / runs);
+	printf("Total time elapsed: %f ms\n", total_time_ms);
+	printf("Average time elapsed: %.10f ms\n", total_time_ms / runs);
 
 	printf("\n\n");
 }
